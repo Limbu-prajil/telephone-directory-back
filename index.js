f2a43e6405251dab440dc80a2ec38519677ae9a4
pralim <limbuprajil123@gmail.com>
2023-05-24 15:24:18 +0300

    express staic build fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
 const cors = require('cors')
 app.use(cors())
 const bodyParser = require('body-parser')
@@ -40,10 +41,6 @@ const formatPerson = (person) => {
     name: person.name,
     number: person.number
   }
-}
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'))
 }
 
 app.get('/api', (req, res) => {
